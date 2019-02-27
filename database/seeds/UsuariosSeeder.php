231f4d8028837d47e10be03c37a42efabbbf664e
ricardo77a <ricardo_77a@hotmail.com>
2019-02-27 17:48:50 -0600

    Update UsuariosSeeder.php

--- database/seeds/UsuariosSeeder.php
+++ database/seeds/UsuariosSeeder.php
@@ -23,5 +23,65 @@ class UsuariosSeeder extends Seeder
 		'created_at'=>'2019-02-26 00:00:00',
 		'updated_at'=>'2019-02-26 00:00:00'
 		] );
+
+		User::create( [
+		'id'=>2,
+		'name'=>'Ricardo Arredondo Ríos',
+		'password' => bcrypt('123qwe'),
+		'email'=>'ricardo@admin.com',
+		'tipo'=>'administrador',
+		'remember_token'=>NULL,
+		'email_verified_at'=>NULL,
+		'created_at'=>'2019-02-26 00:00:00',
+		'updated_at'=>'2019-02-26 00:00:00'
+		] );
+
+		User::create( [
+		'id'=>3,
+		'name'=>'Juan Carlos Soto',
+		'password' => bcrypt('123qwe'),
+		'email'=>'carlos@admin.com',
+		'tipo'=>'administrador',
+		'remember_token'=>NULL,
+		'email_verified_at'=>NULL,
+		'created_at'=>'2019-02-26 00:00:00',
+		'updated_at'=>'2019-02-26 00:00:00'
+		] );
+
+		User::create( [
+		'id'=>4,
+		'name'=>'Pako',
+		'password' => bcrypt('123qwe'),
+		'email'=>'pako@admin.com',
+		'tipo'=>'administrador',
+		'remember_token'=>NULL,
+		'email_verified_at'=>NULL,
+		'created_at'=>'2019-02-26 00:00:00',
+		'updated_at'=>'2019-02-26 00:00:00'
+		] );
+
+		User::create( [
+		'id'=>5,
+		'name'=>'Genny',
+		'password' => bcrypt('123qwe'),
+		'email'=>'genny@admin.com',
+		'tipo'=>'administrador',
+		'remember_token'=>NULL,
+		'email_verified_at'=>NULL,
+		'created_at'=>'2019-02-26 00:00:00',
+		'updated_at'=>'2019-02-26 00:00:00'
+		] );
+
+		User::create( [
+		'id'=>6,
+		'name'=>'Isbac',
+		'password' => bcrypt('123qwe'),
+		'email'=>'isbac@admin.com',
+		'tipo'=>'administrador',
+		'remember_token'=>NULL,
+		'email_verified_at'=>NULL,
+		'created_at'=>'2019-02-26 00:00:00',
+		'updated_at'=>'2019-02-26 00:00:00'
+		] );
     }
 }
