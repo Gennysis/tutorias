4849029d999ca6376cc9124e69dfa44e4676a4a9
ricardo77a <ricardo_77a@hotmail.com>
2019-02-27 19:55:03 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,23 +21,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('auth/{provider}', 'Auth\LoginController@redirectToProvider');
 Route::get('auth/{provider}/callback/', 'Auth\LoginController@handleProviderCallback');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 Route::resource('administrador/usuarios', 'Administrador\UsuariosController');
 Route::resource('administrador/alumnos', 'Administrador\AlumnosController');
 Route::resource('administrador/tutores', 'Administrador\TutoresController');
@@ -46,6 +29,5 @@ Route::resource('administrador/carreras', 'Administrador\CarrerasController');
 Route::resource('administrador/preguntas', 'Administrador\PreguntasController');
 Route::resource('administrador/respuestas', 'Administrador\RespuestasController');
 Route::resource('administrador/ejercicios', 'Administrador\EjerciciosController');
-
-Route::resource('encuestas','Administrador\EncuestasController');
+Route::resource('administrador/encuestas','Administrador\EncuestasController');
 
